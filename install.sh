3f0597fd55119f711e9ba6c14e0a182a61cb47d5
Saghya <saghyattila77@gmail.com>
2021-12-26 22:51:46 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,29 @@
 #/bin/sh
 
-read name
-
-pacman -S --needed base-devel xorg polkit firefox dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa
+sudo pacman -S --needed base-devel xorg polkit firefox dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa
 
 # yay
-git clone https://aur.archlinux.org/yay.git /home/$name && cd /home/$name/yay && makepkg -si
+git clone https://aur.archlinux.org/yay.git ~/ && cd ~/yay && makepkg -si
 # dotfiles
-git clone https://github.com/Saghya/dotfiles /home/$name/
+git clone https://github.com/Saghya/dotfiles ~/
 # dwm
-git clone https://github.com/Saghya/dwm /home/$name/.config/dwm && cd /home/$name/.config/dwm && make && make install
+git clone https://github.com/Saghya/dwm ~/.config/dwm && cd ~/.config/dwm && make && sudo make install
+# dwmblocks
+git clone https://github.com/ashish-yadav11/dwmblocks ~/.config/dwmblocks && cd ~/.config/dwmblocks && make && sudo make install
 # dmenu
-git clone https://github.com/Saghya/dmenu /home/$name/.config/dmenu && cd /home/$name/.config/dmenu && make && make install
+git clone https://github.com/Saghya/dmenu ~/.config/dmenu && cd ~/.config/dmenu && make && sudo make install
 # st
-git clone https://github.com/Saghya/st /home/$name/.config/st && cd /home/$name/.config/st && make && make install
+git clone https://github.com/Saghya/st ~/.config/st && cd ~/.config/st && make && sudo make install
 
-# yay -S pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono ly
-# sytemctl enable ly.service
+yay -S --needed pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono slock
+
+sudo /usr/share/xsessions/dwm.desktop < "[Desktop Entry]
+Encoding=UTF-8
+Name=dwm
+Comment=Dynamic window manager
+Exec=dwm
+Icon=dwm
+Type=XSession"
+
+yay -S ly && sytemctl enable ly.service
 
