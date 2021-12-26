960e3602ef498f55b5b7f55a0667d979656b784e
Saghya <saghyattila77@gmail.com>
2021-12-26 22:01:28 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 read name
 
-pacman -S --needed base-devel xorg polkit dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa
+pacman -S --needed base-devel xorg polkit firefox dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa
 
 # yay
 git clone https://aur.archlinux.org/yay.git /home/$name && cd /home/$name/yay && makepkg -si
 # dotfiles
 git clone https://github.com/Saghya/dotfiles /home/$name/
 # dwm
-git clone https://github.com/Saghya/dwm /home/$name/config && cd /home/$name/.config/dwm && make && make install
+git clone https://github.com/Saghya/dwm /home/$name/.config && cd /home/$name/.config/dwm && make && make install
 # dmenu
 git clone https://github.com/Saghya/dmenu /home/$name/.config && cd /home/$name/.config/dmenu && make && make install
 # st
 git clone https://github.com/Saghya/st /home/$name/.config && cd /home/$name/.config/st && make && make install
 
-# yay -S pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono
+# yay -S pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono ly
+# sytemctl enable ly.service
 
