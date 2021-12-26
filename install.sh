8d7d1dc7b93d6d2f104ca838fb02394f1d470a8a
Saghya <saghyattila77@gmail.com>
2021-12-26 21:28:59 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #/bin/sh
 
-pacman -S --needed base-devel git xorg dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa 
+read name
+
+pacman -S --needed base-devel xorg polkit dunst feh zsh zsh-autosuggestions zsh-syntax-highlighting scrot vim neovim xbindkeys picom alacritty lxappearance arc-gtk-theme arc-icon-theme xterm ranger pcmanfm polkit pavucontrol exa ly
+
+systemctl enable ly.service
 
 # yay
-git clone https://aur.archlinux.org/yay.git ~/ && cd ~/yay && makepkg -si
+git clone https://aur.archlinux.org/yay.git /home/$name && cd /home/$name/yay && makepkg -si
 # dotfiles
-git clone https://github.com/Saghya/dotfiles ~/
+git clone https://github.com/Saghya/dotfiles /home/$name/
 # dwm
-git clone https://github.com/Saghya/dwm ~/.config && cd ~/.config/dwm && make && make install
+git clone https://github.com/Saghya/dwm /home/$name/config && cd /home/$name/.config/dwm && make && make install
 # dmenu
-git clone https://github.com/Saghya/dmenu ~/.config && cd ~/.config/dmenu && make && make install
+git clone https://github.com/Saghya/dmenu /home/$name/.config && cd /home/$name/.config/dmenu && make && make install
 # st
-git clone https://github.com/Saghya/st ~/.config && cd ~/.config/st && make && make install
+git clone https://github.com/Saghya/st /home/$name/.config && cd /home/$name/.config/st && make && make install
 
-yay -S pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono
+# yay -S pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono
 
