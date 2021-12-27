ca99394925e35221e760a9584cf7f497ce4b0aee
Saghya <saghyattila77@gmail.com>
2021-12-27 19:37:01 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+[ -f ~/.install-errors.log ] && rm ~/.install-errors.log
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 sudo grep -q "ILoveCandy" /etc/pacman.conf || sudo sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 sudo sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
@@ -8,10 +9,10 @@ sudo sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/
 
 PCKGS="base-devel clang xorg xorg-xinit xclip xbindkeys polkit man-db pipewire pipewire-pulse pamixer pavucontrol 
     udiskie alacritty noto-fonts noto-fonts-emoji firefox libnotify dunst feh dash zsh zsh-autosuggestions
-    zsh-syntax-highlighting scrot slock vim neovim picom lxappearance arc-gtk-theme arc-icon-theme ranger pcmanfm
-    zathura zathura-pdf-mupdf exa ripgrep fd inetutils"
+    zsh-syntax-highlighting scrot slock vim neovim picom lxappearance arc-gtk-theme arc-icon-theme ueberzug ranger
+    pcmanfm zathura zathura-pdf-mupdf exa ripgrep fd inetutils"
 for PCKG in $PCKGS; do
-    sudo pacman --needed --noconfirm -S "$PCKG" || echo "Error installing $PCKG" >> ~/.install-errors
+    sudo pacman --needed --noconfirm -S "$PCKG" || echo "Error installing $PCKG" >> ~/.install-errors.log
 done
 
 # Use all cores for compilation.
@@ -21,27 +22,27 @@ sudo sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 # yay
 git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg --noconfirm -si ||
-    echo "Error installing yay" >> ~/.install-errors
+    echo "Error installing yay" >> ~/.install-errors.log
 
 AUR_PCKGS="pfetch breeze-snow-cursor-theme nerd-fonts-jetbrains-mono htop-vim ly batsignal dashbinsh"
 for PCKG in $AUR_PCKGS; do
-    yay --needed --noconfirm -S "$PCKG" || echo "Error installing $PCKG" >> ~/.install-errors
+    yay --needed --noconfirm -S "$PCKG" || echo "Error installing $PCKG" >> ~/.install-errors.log
 done
 
 ## GIT PACKAGES
 
 # dotfiles
 git clone --bare https://github.com/Saghya/dotfiles ~/.dotfiles && /usr/bin/git --git-dir="$HOME"/.dotfiles/ \
-    --work-tree="$HOME" checkout || echo "Error installing dotfiles" >> ~/.install-errors
+    --work-tree="$HOME" checkout || echo "Error installing dotfiles" >> ~/.install-errors.log
 
 # dwm
 git clone https://github.com/Saghya/dwm ~/.config/dwm && cd ~/.config/dwm && make && sudo make install ||
-    echo "Error installing dwm" >> ~/.install-errors
+    echo "Error installing dwm" >> ~/.install-errors.log
 
 # dwmblocks
 git clone https://github.com/ashish-yadav11/dwmblocks ~/.config/dwmblocks && cd ~/.config/dwmblocks && make &&
     sudo make install && cp ~/.scripts/blocks/* ~/.config/dwmblocks/blocks && 
-    sed -i "$(( $(wc -l <~/.config/dwmblocks/config.def.h)-8+1 )),$ d" ~/.config/dwmblocks/config.def.h &&
+    sed -i "$(( $(wc -l <~/.config/dwmblocks/config.h)-8+1 )),$ d" ~/.config/dwmblocks/config.h &&
     echo "static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
         { PATH("volume"),               PATH("volume-button"),          0,              1},
@@ -52,12 +53,17 @@ git clone https://github.com/ashish-yadav11/dwmblocks ~/.config/dwmblocks && cd 
         { PATH("date"),                 NULL,                           5,              6},
         { PATH("powermenu_icon"),       PATH("powermenu"),              0,              7},
         { NULL } /* just to mark the end of the array */
-};" >> config.def.h && sudo make install ||
-    echo "Error installing dwmblocks" >> ~/.install-errors
+};" >> config.h && sudo make install ||
+    echo "Error installing dwmblocks" >> ~/.install-errors.log
 
 # dmenu
 git clone https://github.com/Saghya/dmenu ~/.config/dmenu && cd ~/.config/dmenu && make && sudo make install ||
-    echo "Error installing dmenu" >> ~/.install-errors
+    echo "Error installing dmenu" >> ~/.install-errors.log
+
+# neovim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' ||
+       echo "Error installing neovim-plug" >> ~/.install-errors.log
 
 # dwm login session
 sudo mkdir -p /usr/share/xsessions
@@ -71,15 +77,15 @@ Icon=dwm
 Type=XSession' | sudo tee /usr/share/xsessions/dwm.desktop
 
 # display manager
-sudo systemctl enable ly.service || echo "Error enabling display manager" >> ~/.install-errors
+sudo systemctl enable ly.service || echo "Error enabling display manager" >> ~/.install-errors.log
 
 # default shell
-chsh -s /usr/bin/zsh || echo "Error changing default shell" >> ~/.install-errors
+chsh -s /usr/bin/zsh || echo "Error changing default shell" >> ~/.install-errors.log
 # relinking /bin/sh
-sudo ln -sfT dash /usr/bin/sh || echo "Error relinking /bin/sh" >> ~/.install-errors
+sudo ln -sfT dash /usr/bin/sh || echo "Error relinking /bin/sh" >> ~/.install-errors.log
 
 # errors
-[ -f ~/.install-errors ] && echo "
+[ -f ~/.install-errors.log ] && echo "
 ######################
-ERRORS:" && cat ~/.install-errors && echo "######################"
+ERRORS:" && cat ~/.install-errors.log && echo "######################"
 
