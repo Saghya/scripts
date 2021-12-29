0183313bba72c46fd5334241277f04926eb6fb91
Saghya <saghyattila77@gmail.com>
2021-12-29 19:06:38 +0100

    changes

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ sudo sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/
 ## PACKAGES
 
 PCKGS="base-devel xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xrandr xorg-xdpyinfo xorg-xbacklight
-    xclip xdotool xbindkeys man-db polkit acpid pipewire pipewire-pulse pavucontrol udiskie alacritty noto-fonts
-    noto-fonts-emoji firefox libnotify dunst feh dash zsh zsh-autosuggestions zsh-syntax-highlighting scrot slock
-    vim neovim picom lxappearance gtk-engine-murrine gnome-themes-extra arc-gtk-theme arc-icon-theme ueberzug ranger
-    pcmanfm zathura zathura-pdf-mupdf exa inetutils ripgrep fd clang pyright tlp"
+    xclip xdotool xbindkeys xdg-utils man-db polkit acpid pipewire pipewire-pulse pavucontrol udiskie alacritty
+    noto-fonts noto-fonts-emoji firefox libnotify dunst feh dash zsh zsh-autosuggestions zsh-syntax-highlighting
+    scrot slock vim neovim picom lxappearance gtk-engine-murrine gnome-themes-extra arc-gtk-theme arc-icon-theme
+    ueberzug ranger pcmanfm zathura zathura-pdf-mupdf exa inetutils ripgrep fd clang pyright tlp"
 for PCKG in $PCKGS; do
     sudo pacman --needed --noconfirm -S "$PCKG" || echo "Error installing $PCKG" >> ~/.install-errors.log
 done
@@ -46,9 +46,9 @@ git clone https://github.com/Saghya/dwm ~/.local/src/dwm && cd ~/.local/src/dwm 
 
 # dwmblocks
 git clone https://github.com/ashish-yadav11/dwmblocks ~/.local/src/dwmblocks && cd ~/.local/src/dwmblocks &&
-    cp -r ~/.local/scripts/blocks ~/.local/src/dwmblocks && make && sudo make install && sed -i "20,22d" config.h && 
-    sed -i "$(( $(wc -l <~/.local/src/dwmblocks/config.h)-8+1 )),$ d" ~/.local/src/dwmblocks/config.h &&
-    echo "static const char delimiter[] = { ' ', '|', ' ', DELIMITERENDCHAR };
+cp -r ~/.local/scripts/blocks ~/.local/src/dwmblocks && make && sudo make install && sed -i "20,22d" config.h && 
+sed -i "$(( $(wc -l <~/.local/src/dwmblocks/config.h)-8+1 )),$ d" ~/.local/src/dwmblocks/config.h &&
+echo "static const char delimiter[] = { ' ', '|', ' ', DELIMITERENDCHAR };
 #include \"block.h\"
 static Block blocks[] = {
 /*      pathu                           pathc                           interval        signal */
@@ -68,13 +68,14 @@ git clone https://github.com/Saghya/dmenu ~/.local/src/dmenu && cd ~/.local/src/
     echo "Error installing dmenu" >> ~/.install-errors.log
 
 # touchpad
-sudo touch /etc/X11/xorg.conf.d/30-touchpad.conf
+sudo touch /etc/X11/xorg.conf.d/30-touchpad.conf &&
 echo "Section \"InputClass\"
     Identifier \"devname\"
     Driver \"libinput\"
     Option \"Tapping\" \"on\"
     Option \"NaturalScrolling\" \"true\"
-EndSection" | sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf
+EndSection" | sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf ||
+    echo "Error configuring touchpad" >> ~/.install-errors.log
 
 # acpi events
 sudo systemctl enable acpid.service || echo "Error enabling acpid.service" >> ~/.install-errors.log
@@ -85,13 +86,13 @@ echo "Error creating jack event" >> ~/.install-errors.log
 sudo touch /etc/acpi/events/ac_adapter &&
 echo "event=ac_adapter
 action=pkill -RTMIN+4 dwmblocks" | sudo tee /etc/acpi/events/ac_adapter ||
-echo "Error creating ac_adapter event" >> ~/.install-errors.log
+    echo "Error creating ac_adapter event" >> ~/.install-errors.log
 
 # allow changing brightness for users in video group
 sudo touch /etc/udev/rules.d/backlight.rules &&
 echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"acpi_video0\", GROUP=\"video\", MODE=\"0664\"" |
 sudo tee /etc/udev/rules.d/backlight.rules ||
-echo "Error allowing brightness changing" >> ~/.install-erros.log
+    echo "Error allowing brightness changing" >> ~/.install-erros.log
 
 # dwm login session
 sudo mkdir -p /usr/share/xsessions &&
@@ -103,10 +104,14 @@ Comment=Dynamic window manager
 Exec=startdwm
 Icon=dwm
 Type=XSession" | sudo tee /usr/share/xsessions/dwm.desktop ||
-echo "Error creating dwm login session" >> ~/.install-errors.log
+    echo "Error creating dwm login session" >> ~/.install-errors.log
 
 # display manager
-sudo systemctl enable ly.service || echo "Error enabling display manager" >> ~/.install-errors.log
+sudo systemctl enable ly.service &&
+echo "term_reset_cmd = /usr/bin/tput reset; /usr/bin/printf '%b' '\e]P0222430\e]P769a2ff\ec'" |
+sudo tee /etc/ly/config.ini &&
+sudo sed -i "8i ExecStartPre=/usr/bin/printf '%%b' '\e]P0222430\e]P769a2ff\ec'" /lib/systemd/system/ly.service ||
+    echo "Error installing display manager" >> ~/.install-errors.log
 
 # default shell
 chsh -s /usr/bin/zsh || echo "Error changing default shell" >> ~/.install-errors.log
@@ -114,7 +119,7 @@ chsh -s /usr/bin/zsh || echo "Error changing default shell" >> ~/.install-errors
 sudo ln -sfT dash /usr/bin/sh || echo "Error relinking /bin/sh" >> ~/.install-errors.log
 
 # errors
-[ -f ~/.install-errors.log ] && echo "
+[ -f ~/.install-errors.log ] && echo "\033[1;31m
 ######################
-ERRORS:" && cat ~/.install-errors.log && echo "######################"
+ERRORS:" && cat ~/.install-errors.log && echo "######################\033[0m"
 
