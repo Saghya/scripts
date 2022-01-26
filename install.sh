f4864d0782be64b5e5e4f08d5b0b0e2b6b9dfb7c
Saghya <saghyattila77@gmail.com>
2022-01-26 23:13:57 +0100

    switched scrot to maim

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ mkdir -p ~/.local/src
 PCKGS="base-devel xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xrandr xorg-xdpyinfo xclip xdotool
     xbindkeys xdg-utils man-db polkit acpid pipewire pipewire-pulse pavucontrol pamixer wget udiskie
     alacritty noto-fonts noto-fonts-emoji chromium dunst feh dash zsh zsh-autosuggestions zsh-syntax-highlighting
-    scrot vim neovim picom lxappearance gtk-engine-murrine gnome-themes-extra arc-gtk-theme papirus-icon-theme
+    maim vim neovim picom lxappearance gtk-engine-murrine gnome-themes-extra arc-gtk-theme papirus-icon-theme
     ueberzug ranger pcmanfm zathura zathura-pdf-mupdf mpv exa inetutils ripgrep fd clang pyright bluez bluez-utils"
 sudo pacman --noconfirm -Syyu
 for PCKG in $PCKGS; do
