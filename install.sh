446e499a9da0a4dd2968d78c1acc9e1452a70410
Saghya <saghyattila77@gmail.com>
2022-03-07 22:58:36 +0100

    added man-pages

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ mkdir -p ~/.local/src
 ## PACKAGES ##
 
 PCKGS="base-devel xorg-server xorg-xwininfo xorg-xinit xorg-xprop xorg-xrandr xorg-xdpyinfo xclip xdotool
-    xbindkeys xdg-utils man-db polkit acpid pipewire pipewire-pulse pavucontrol pamixer wget udiskie
+    xbindkeys xdg-utils man-db man-pages polkit acpid pipewire pipewire-pulse pavucontrol pamixer wget udiskie
     alacritty noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji chromium dunst feh dash zsh
     zsh-autosuggestions zsh-syntax-highlighting maim vim neovim picom lxappearance gtk-engine-murrine
     gnome-themes-extra arc-gtk-theme papirus-icon-theme ueberzug ranger pcmanfm zathura zathura-pdf-mupdf
